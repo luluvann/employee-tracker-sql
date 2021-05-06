8e954f0ea21be1a58c6426b303869563ea0f0b9f
Lucile <developer.lvt@gmail.com>
2021-05-06 17:43:58 -0400

    created queries functions

--- connection.js
+++ connection.js
@@ -1,1 +1,17 @@
 require('dotenv').config()
+/*To make the connection works, a .env will be needed with your MySQL password. Declare the following env variable:
+MYSQL_PASS=replacewithyourpassword
+*/
+
+const mysql = require('mysql2/');
+
+const db = await mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: process.env.MYSQL_PASS,
+  database: 'team'
+});
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,34 +1,75 @@
 const inquirer = require("inquirer");
-/* const db = require("./db/db.sql"); */
+const db = require("./connection");
+const cTable = require("console.table");
 
-/* db.query(`SELECT * FROM employees`, (err, rows) => {
-    console.log(rows);
+function viewAllDepartments() {
+  db.query(`SELECT name AS 'department', id FROM employees`, (err, rows) => {
+    const table = cTable.getTable(rows);
+    console.log(table);
   });
+  init();
+}
 
-  console.log("works") */
+function viewAllRoles() {
+  db.query(
+    `SELECT roles.title, roles.id, departments.name AS 'department', roles.salary FROM roles LEFT JOIN departments ON roles.department_id = departments.id`,
+    (err, rows) => {
+      const table = cTable.getTable(rows);
+      console.log(table);
+    }
+  );
+  init();
+}
 
-  const initialQuestion = [
-    {
-        type: "list",
-        name: "action",
-        message: "What would you like to do:",
-        choices: [
-            "view all departments",
-            "view all roles",
-            "view all employees",
-            "add a department",
-            "add a role",
-            "add am employee",
-            "update an employee role"
-        ],
-      },
-  ]
+function viewAllEmployees() {
+  db.query(
+    `SELECT 
+    e.id, 
+    e.first_name, 
+    e.last_name, 
+    roles.title, 
+    departments.name AS 'department',
+    roles.salary,
+    CONCAT(m.first_name,' ' ,m.last_name) AS 'manager' 
+  FROM employees e 
+  LEFT JOIN employees m ON e.manager_id = m.id
+  LEFT JOIN roles ON e.role_id = roles.id 
+  LEFT JOIN departments ON roles.department_id = departments.id`,
+    (err, rows) => {
+      const table = cTable.getTable(rows);
+      console.log(table);
+    }
+  );
+  init();
+}
 
-  function init() {
-    return inquirer.prompt([...initialQuestion]);
+const initialQuestion = [
+  {
+    type: "list",
+    name: "action",
+    message: "What would you like to do:",
+    choices: [
+      "view all departments",
+      "view all roles",
+      "view all employees",
+      "add a department",
+      "add a role",
+      "add am employee",
+      "update an employee role",
+    ],
+  },
+];
+
+function init() {
+  return inquirer.prompt([...initialQuestion]);
+}
+
+init().then((data) => {
+  if (data.action === "view all employees") {
+    viewAllEmployees();
+  } else if (data.action === "view all roles") {
+    viewAllRoles();
+  } else if (data.action === "view all departments") {
+    viewAllDepartments();
   }
-  
-  init()
-  .then((data) => {
-      console.log(data)
-  })
+});
