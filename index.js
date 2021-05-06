d68ca82d6e1d604cab7cfe94c00121f2a0a38320
Lucile <developer.lvt@gmail.com>
2021-05-05 21:56:14 -0400

    added inquirer

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-const db = require("./db/db.sql")
+const inquirer = require("inquirer");
+/* const db = require("./db/db.sql"); */
 
-db.query(`SELECT * FROM employees`, (err, rows) => {
+/* db.query(`SELECT * FROM employees`, (err, rows) => {
     console.log(rows);
   });
 
-  console.log("works")
+  console.log("works") */
+
+  const initialQuestion = [
+    {
+        type: "list",
+        name: "action",
+        message: "What would you like to do:",
+        choices: [
+            "view all departments",
+            "view all roles",
+            "view all employees",
+            "add a department",
+            "add a role",
+            "add am employee",
+            "update an employee role"
+        ],
+      },
+  ]
+
+  function init() {
+    return inquirer.prompt([...initialQuestion]);
+  }
+  
+  init()
+  .then((data) => {
+      console.log(data)
+  })
