dc228c0d33b386d924228e0f396282232ed540a0
Lucile <developer.lvt@gmail.com>
2021-05-08 13:01:02 -0400

    refactored by moving questions to a dedicated file

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const inquirer = require("inquirer");
 const db = require("./connection");
 const cTable = require("console.table");
 
+/* import questions */
+const questions = require("./questions");
+
+const initialQuestion = questions.initialQuestion;
+const addRoleQuestions = questions.addRoleQuestions;
+const addEmployeeQuestions = questions.addEmployeeQuestions;
+const updateEmployeeRoleQuestions = questions.updateEmployeeRoleQuestions
+
 /* Query functions */
 /* view tables functions*/
 async function viewAllDepartments() {
@@ -240,90 +248,6 @@ async function updateEmployeeRole(updateEmployeeRoleQuestions) {
     prompt();
   });
 }
-
-/* Questions */
-const initialQuestion = [
-  {
-    type: "list",
-    name: "action",
-    message: "What would you like to do: ",
-    choices: [
-      "view all departments",
-      "view all roles",
-      "view all employees",
-      "add a department",
-      "add a role",
-      "add an employee",
-      "update an employee role",
-      "exit",
-    ],
-  },
-];
-
-const addRoleQuestions = [
-  {
-    type: "input",
-    name: "title",
-    message: "What is the role's title?: ",
-  },
-  {
-    type: "input",
-    name: "salary",
-    message: "What is the role's salary?: ",
-  },
-  {
-    type: "list",
-    name: "department",
-    message: "Select the department which the role belongs to: ",
-    choices: [""],
-  },
-];
-
-const addEmployeeQuestions = [
-  {
-    type: "input",
-    name: "firstName",
-    message: "What is the new employee's first name?: ",
-  },
-  {
-    type: "input",
-    name: "lastName",
-    message: "What is the new employee's last name?: ",
-  },
-  {
-    type: "list",
-    name: "role",
-    message: "What role will the new employee have?: ",
-    choices: [""],
-  },
-  {
-    type: "list",
-    name: "manager",
-    message: "Who will the new employee report to?: ",
-    choices: ["none"],
-  },
-];
-
-const updateEmployeeRoleQuestions = [
-  {
-    type: "input",
-    name: "test",
-    message: "",
-  },
-
-  {
-    type: "list",
-    name: "employeeToUpdate",
-    message: "Select the employee you'd like to modify: ",
-    choices: [""],
-  },
-  {
-    type: "list",
-    name: "role",
-    message: "Select the new role of the employee: ",
-    choices: [""],
-  },
-];
 
 /* Prompts function*/
 function prompt() {
